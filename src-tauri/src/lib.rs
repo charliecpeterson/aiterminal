f92cfbf7ef15d206d168ffc13a985a808bf8e673
charliecpeterson <charliecpeterson@gmail.com>
2025-12-17 15:42:31 -0800

    Run helper then login shell to keep colors

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -201,10 +201,10 @@ aiterm_ssh() {
     command ssh -tt "${ssh_opts[@]}" "$@" 'remote_shell="${SHELL:-/bin/sh}";
         case "$remote_shell" in
             */bash)
-                exec env TERM_PROGRAM=aiterminal SHELL="$remote_shell" "$remote_shell" -i --rcfile <(cat ~/.config/aiterminal/bash_init.sh ~/.bashrc ~/.bash_profile ~/.profile 2>/dev/null)
+                exec env TERM_PROGRAM=aiterminal SHELL="$remote_shell" "$remote_shell" -l -c "source ~/.config/aiterminal/bash_init.sh; exec $remote_shell -l"
                 ;;
             */zsh)
-                exec env TERM_PROGRAM=aiterminal SHELL="$remote_shell" "$remote_shell" -i -c "source ~/.zprofile 2>/dev/null; source ~/.zshrc 2>/dev/null; source ~/.config/aiterminal/bash_init.sh; exec \"$remote_shell\" -i"
+                exec env TERM_PROGRAM=aiterminal SHELL="$remote_shell" "$remote_shell" -l -c "source ~/.config/aiterminal/bash_init.sh; exec $remote_shell -l"
                 ;;
             *)
                 exec "$remote_shell" -l
